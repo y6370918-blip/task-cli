1dd61d99b740dd061c96e7b277eb168d4ea3e179
bolanqiu <y6370918-blip@users.noreply.github.com>
2026-07-16 00:23:43 +0800

    feat: add pydantic task schemas

---
+++ src/task_cli/schemas.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+TaskStatus = Literal[
+    "pending",
+    "doing",
+    "done",
+]
+
+class TaskCreate(BaseModel):
+
+    title: str = Field(
+        min_length=1,
+        max_length=100,
+    )
+
+    description: str = Field(
+        default=None,
+        max_length=500,
+    )
+
+class TaskUpdate(BaseModel):
+    title: str | None = Field(
+        default=None,
+        min_length=1,
+        max_length=100,
+    )
+
+    description: str | None = None
+
+    status: TaskStatus | None = None
+
+class TaskRead(BaseModel):
+
+    id: int
+    title: str
+    description: str | None
+    status: TaskStatus
+    created_at: datetime
+    updated_at: datetime
---
+++ tests/test_models.py
@@ -0,0 +1,35 @@
+import pytest
+from pydantic import ValidationError
+from task_cli.schemas import TaskCreate, TaskUpdate, TaskRead
+
+def test_create_task_success():
+    task=TaskCreate(
+        title="学习pydantic"
+    )
+    assert task.title == "学习pydantic"
+
+def test_title_cannot_be_empty():
+    with pytest.raises(ValidationError):
+        TaskCreate(
+            title=""
+        )
+
+def test_title_lenth_limit():
+    with pytest.raises(ValidationError):
+        TaskCreate(
+            title="A"*101
+        )
+
+def test_description_can_be_none():
+    task=TaskCreate(
+        title="测试"
+    )
+
+    assert task.description is None
+
+def test_status_vaildation():
+    task=TaskUpdate(
+        status="done"
+    )
+    
+    assert task.status == "done"
